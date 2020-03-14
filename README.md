b3d147e3a2747bde1f5c20e7189f7fb661eb8fa5
Rijin Lee <rijinlee@gmail.com>
2020-03-14 16:48:25 -0400

    excel included

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # topMoviesFinder
 
-Finds the top 200 movies rated by IMDB and lists them onto an excel spreadsheet.
-I wanted to work with a module that allows me to write to an excel file.
+Finds the top 100 movies rated by Rotten Tomatoes, scrapes data, and lists them onto an excel spreadsheet.
+I wanted to work with a module that allows me to scrape data from a website and write onto an excel file.
--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 console.log("Finding Top Movies");
-const request = require("request");
-const cheerio = require("cheerio");
 
 // parse the HTML body that request operation rececived
 // use the cheerio package to achieve this
 function parseRottenTomatoesHTML(fileData) {
+  const cheerio = require("cheerio");
   // prep the return obj
   let allTomatoData = {
     top100: []
@@ -23,9 +22,8 @@ function parseRottenTomatoesHTML(fileData) {
     let top100Doc = cheerio.load(fullDoc('[id = "top_movies_main"]').html());
 
     // iterate through movies and strip useful parts, add each to return obj
-    top100Doc("tr").each(function(index, element) {
+    top100Doc("tr").each(function(index) {
       let movieDoc = cheerio.load(top100Doc(this).html());
-      // console.log("this is the movieDoc: ", movieDoc);
       let movieRank = index;
       // movieDoc(".bold").text().trim();
       let movieRating = movieDoc(".tMeterScore")
@@ -40,7 +38,7 @@ function parseRottenTomatoesHTML(fileData) {
         rating: movieRating,
         title: movieTitie
       };
-      console.log("this is the movieobj: ", movieObj);
+      // console.log("this is the movieobj: ", movieObj);
       allTomatoData.top100[allTomatoData.top100.length] = movieObj;
     });
   }
@@ -52,9 +50,10 @@ const TOP100_URL = "https://www.rottentomatoes.com/top/bestofrt/?year=";
 const prevYear = new Date().getFullYear() - 1;
 const ROTTEN_TOMATOES_TOP100_URL = `${TOP100_URL}${prevYear}`;
 
-// makes a call to get the HTML from the rotten tomatoes front page
+// makes a call to get the HTML from the rotten tomatoes top100 page
 // uses the request package to achieve this
 function requestRottenTomatoesHTML(callback) {
+  const request = require("request");
   request({ uri: ROTTEN_TOMATOES_TOP100_URL }, function(error, response, body) {
     if (!error) {
       let parsedData = parseRottenTomatoesHTML(body);
@@ -69,9 +68,47 @@ function requestRottenTomatoesHTML(callback) {
   });
 }
 
+//
 requestRottenTomatoesHTML(function(error, data) {
   if (!error) {
-    console.log(JSON.stringify(data, null, 2));
+    // console.log(JSON.stringify(data, null, 2));
+    const xl = require("excel4node");
+
+    //////////////Excel Code///////////////
+    // Create a new instance of a Workbook class
+    let wb = new xl.Workbook();
+
+    // Add Worksheets to the workbook
+    var ws = wb.addWorksheet(`Rotten Tomatoes Top100 of ${prevYear}`);
+
+    // Create a reusable style
+    var style = wb.createStyle({
+      font: {
+        color: "#000000",
+        size: 12
+      },
+      numberFormat: "#,##0; (#,##0); -"
+    });
+
+    ws.cell(1 + 1, 2)
+      .string("My simple string")
+      .style(style);
+
+    for (var i = 0; i < 101; i++) {
+      ws.cell(i + 1, 1)
+        .number(data.top100[i].rank)
+        .style(style); // rank
+
+      ws.cell(i + 1, 2)
+        .string(data.top100[i].rating)
+        .style(style); // artist name
+
+      ws.cell(i + 1, 3)
+        .string(data.top100[i].title)
+        .style(style); // song title
+    }
+
+    wb.write(`rottenTomatoesTop100${prevYear}.xlsx`);
   } else {
     console.log(`some error occured`);
   }
